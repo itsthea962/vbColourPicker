e862b38f54f7726810fdf928e7f408a91a1f34be
theo3334 <47482851+theo3334@users.noreply.github.com>
2020-10-21 14:40:21 +0100

    v1.4.00.25 (BETA)
    Adds window sizing, saved state of backgound colour changing and opacity changibility. Validation will be added along with new features later.

---
+++ Form2.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form2))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.backgroundColour = New System.Windows.Forms.Timer(Me.components)
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButtonSmall = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonNormal = New System.Windows.Forms.RadioButton()
+        Me.RadioButtonSmaller = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.opacityText = New System.Windows.Forms.TextBox()
+        Me.opacitySet = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(142, 12)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 12)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 21)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Background Colour:"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'backgroundColour
+        '
+        Me.backgroundColour.Enabled = True
+        Me.backgroundColour.Interval = 1
+        '
+        'Label2
+        '
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 66)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 21)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Size Of Window:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'RadioButtonSmall
+        '
+        Me.RadioButtonSmall.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButtonSmall.Location = New System.Drawing.Point(142, 66)
+        Me.RadioButtonSmall.Name = "RadioButtonSmall"
+        Me.RadioButtonSmall.Size = New System.Drawing.Size(73, 21)
+        Me.RadioButtonSmall.TabIndex = 3
+        Me.RadioButtonSmall.TabStop = True
+        Me.RadioButtonSmall.Text = "Small"
+        Me.RadioButtonSmall.UseVisualStyleBackColor = True
+        '
+        'RadioButtonNormal
+        '
+        Me.RadioButtonNormal.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButtonNormal.Location = New System.Drawing.Point(142, 93)
+        Me.RadioButtonNormal.Name = "RadioButtonNormal"
+        Me.RadioButtonNormal.Size = New System.Drawing.Size(73, 21)
+        Me.RadioButtonNormal.TabIndex = 4
+        Me.RadioButtonNormal.TabStop = True
+        Me.RadioButtonNormal.Text = "Normal"
+        Me.RadioButtonNormal.UseVisualStyleBackColor = True
+        '
+        'RadioButtonSmaller
+        '
+        Me.RadioButtonSmaller.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButtonSmaller.Location = New System.Drawing.Point(142, 39)
+        Me.RadioButtonSmaller.Name = "RadioButtonSmaller"
+        Me.RadioButtonSmaller.Size = New System.Drawing.Size(73, 21)
+        Me.RadioButtonSmaller.TabIndex = 5
+        Me.RadioButtonSmaller.TabStop = True
+        Me.RadioButtonSmaller.Text = "Smaller"
+        Me.RadioButtonSmaller.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(10, 120)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 25)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Opacity:"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'opacityText
+        '
+        Me.opacityText.AcceptsReturn = True
+        Me.opacityText.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.opacityText.Location = New System.Drawing.Point(140, 120)
+        Me.opacityText.Name = "opacityText"
+        Me.opacityText.Size = New System.Drawing.Size(75, 25)
+        Me.opacityText.TabIndex = 7
+        '
+        'opacitySet
+        '
+        Me.opacitySet.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.opacitySet.Location = New System.Drawing.Point(222, 120)
+        Me.opacitySet.Name = "opacitySet"
+        Me.opacitySet.Size = New System.Drawing.Size(41, 25)
+        Me.opacitySet.TabIndex = 8
+        Me.opacitySet.Text = "%"
+        Me.opacitySet.UseVisualStyleBackColor = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 410)
+        Me.Controls.Add(Me.opacitySet)
+        Me.Controls.Add(Me.opacityText)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RadioButtonSmaller)
+        Me.Controls.Add(Me.RadioButtonNormal)
+        Me.Controls.Add(Me.RadioButtonSmall)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form2"
+        Me.Text = "Options"
+        Me.TopMost = True
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents backgroundColour As Timer
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButtonSmall As RadioButton
+    Friend WithEvents RadioButtonNormal As RadioButton
+    Friend WithEvents RadioButtonSmaller As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents opacityText As TextBox
+    Friend WithEvents opacitySet As Button
+End Class
---
+++ Form2.vb
@@ -0,0 +1,179 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.MaximumSize = New Size(Width, Height)
+        Me.MinimumSize = Me.MaximumSize
+        Me.MaximizeBox = False
+        'prevents the windows from being resizedackgroundColour.Start()
+        'starts the timer to change the background colour
+
+        ComboBox1.Text = Form1.BackColor.Name
+        'sets the combobox text to the current colour
+
+        Me.MaximizeBox = False
+        'stop the window being maximised in size
+
+        ComboBox1.Items.Add("White")
+        ComboBox1.Items.Add("Silver")
+        ComboBox1.Items.Add("Grey")
+        ComboBox1.Items.Add("Black")
+        ComboBox1.Items.Add("Red")
+        ComboBox1.Items.Add("Maroon")
+        ComboBox1.Items.Add("Yellow")
+        ComboBox1.Items.Add("Olive")
+        ComboBox1.Items.Add("Lime")
+        ComboBox1.Items.Add("Green")
+        ComboBox1.Items.Add("Aqua")
+        ComboBox1.Items.Add("Teal")
+        ComboBox1.Items.Add("Blue")
+        ComboBox1.Items.Add("Navy")
+        ComboBox1.Items.Add("Magenta")
+        ComboBox1.Items.Add("Purple")
+        'cba to do it in one line, fat effort
+        'anyways this block of code is for the combobox dropdown to change the background colour
+
+    End Sub
+
+    Private Sub backgroundColour_Tick(sender As Object, e As EventArgs) Handles backgroundColour.Tick
+
+        If ComboBox1.Text = "White" Then
+
+            Form1.BackColor = Color.White
+            My.Settings.backcolour = Color.White
+
+        ElseIf ComboBox1.Text = "Silver" Then
+
+            Form1.BackColor = Color.Silver
+            My.Settings.backcolour = Color.Silver
+
+        ElseIf ComboBox1.Text = "Grey" Then
+
+            Form1.BackColor = Color.Gray
+            My.Settings.backcolour = Color.Gray
+
+        ElseIf ComboBox1.Text = "Black" Then
+
+            Form1.BackColor = Color.Black
+            My.Settings.backcolour = Color.Black
+
+        ElseIf ComboBox1.Text = "Red" Then
+
+            Form1.BackColor = Color.Red
+            My.Settings.backcolour = Color.Red
+
+        ElseIf ComboBox1.Text = "Maroon" Then
+
+            Form1.BackColor = Color.Maroon
+            My.Settings.backcolour = Color.Maroon
+
+        ElseIf ComboBox1.Text = "Yellow" Then
+
+            Form1.BackColor = Color.Yellow
+            My.Settings.backcolour = Color.Yellow
+
+        ElseIf ComboBox1.Text = "Olive" Then
+
+            Form1.BackColor = Color.Olive
+            My.Settings.backcolour = Color.Olive
+
+        ElseIf ComboBox1.Text = "Lime" Then
+
+            Form1.BackColor = Color.Lime
+            My.Settings.backcolour = Color.Lime
+
+        ElseIf ComboBox1.Text = "Green" Then
+
+            Form1.BackColor = Color.Green
+            My.Settings.backcolour = Color.Green
+
+        ElseIf ComboBox1.Text = "Aqua" Then
+
+            Form1.BackColor = Color.Aqua
+            My.Settings.backcolour = Color.Aqua
+
+        ElseIf ComboBox1.Text = "Teal" Then
+
+            Form1.BackColor = Color.Teal
+            My.Settings.backcolour = Color.Teal
+
+        ElseIf ComboBox1.Text = "Blue" Then
+
+            Form1.BackColor = Color.Blue
+            My.Settings.backcolour = Color.Blue
+
+        ElseIf ComboBox1.Text = "Navy" Then
+
+            Form1.BackColor = Color.Navy
+            My.Settings.backcolour = Color.Navy
+
+        ElseIf ComboBox1.Text = "Magenta" Then
+
+            Form1.BackColor = Color.Magenta
+            My.Settings.backcolour = Color.Magenta
+
+        ElseIf ComboBox1.Text = "Purple" Then
+
+            Form1.BackColor = Color.Purple
+            My.Settings.backcolour = Color.Purple
+
+        End If
+        'the biggest if statement in the world
+        'runs on a timer so every 1ms it will change the form1 backcolour if necessary
+        'and it should also save the colour in the mysettings within backcolour
+
+    End Sub
+
+    Private Sub RadioButtonSmaller_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButtonSmaller.CheckedChanged
+
+        Form1.Height = 198
+        Form1.Width = 314
+        'the height for the smaller size
+
+    End Sub
+
+    Private Sub RadioButtonSmall_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButtonSmall.CheckedChanged
+
+        Form1.Height = 310
+        Form1.Width = 314
+        'the height for the small size
+
+    End Sub
+
+    Private Sub RadioButtonNormal_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButtonNormal.CheckedChanged
+
+        Form1.Height = 318
+        Form1.Width = 595
+        'the height for the smaller size
+
+    End Sub
+
+    Private Sub opacitySet_Click(sender As Object, e As EventArgs) Handles opacitySet.Click
+
+        Form1.Opacity = opacityText.Text / 100
+        'only accepts values from 0 to 1, so this converts it from percentage to decimal
+
+    End Sub
+
+    Private Sub opacityText_KeyPress(sender As Object, e As KeyPressEventArgs) Handles opacityText.KeyPress
+
+        If Asc(e.KeyChar) <> 8 Then
+            If Asc(e.KeyChar) < 48 Or Asc(e.KeyChar) > 57 Then
+                e.Handled = True
+            End If
+        End If
+        'to only allow numbers to be entered
+
+    End Sub
+
+    Private Sub opacityText_KeyDown(sender As Object, e As KeyEventArgs) Handles opacityText.KeyDown
+
+        If e.KeyCode = Keys.Enter Then
+
+            Form1.Opacity = opacityText.Text / 100
+            'only accepts values from 0 to 1, so this converts it from percentage to decimal
+
+        End If
+        'if the enter key was to be pressed
+
+    End Sub
+End Class
